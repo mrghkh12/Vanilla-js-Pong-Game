df383515978ff8450a7af26949012fbb6ed05ea8
mohammad reza <mrghkh12@gmail.com>
2024-07-31 18:31:39 +0330

    feat(ball.js): ball coordinate

--- scripts/ball.js
+++ scripts/ball.js
@@ -1,6 +1,26 @@
 export default class Ball{
     constructor(ballElem){
         this.ballElem = ballElem
+        this.rest()
+    }
+
+    get x(){
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--x'))
+    }
+    set x(value){
+        this.ballElem.style.setProperty('--x' , value)
+    }
+
+    get y(){
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--y'))
+    }
+    set y(value){
+        this.ballElem.style.setProperty('--y' , value)
+    }
+
+    rest(){
+        this.x = 50;
+        this.y = 50; 
     }
 
     update(delta){
