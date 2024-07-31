c997257598b65fca0d1011ce0f98051ea9c63f9b
mohammad reza <mrghkh12@gmail.com>
2024-07-31 17:01:55 +0330

    fix(app.js): clear codes

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,16 @@
-<<<<<<< Updated upstream
-
-
-const $ = document;
-
-=======
-<<<<<<< Updated upstream
-=======
 import Ball from "./ball.js";
 
 const $ = document;
 
 const ball = new Ball($.getElementById('ball'))
 
->>>>>>> Stashed changes
+
 let lastTime = null
 function updateApp(time){
     if(lastTime){
         const delta = time - lastTime
-<<<<<<< Updated upstream
-        console.log(delta);
-=======
         ball.update(delta);
->>>>>>> Stashed changes
+
     }
 
     lastTime = time
@@ -29,7 +18,3 @@ function updateApp(time){
 }
 
 window.requestAnimationFrame(updateApp)
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
