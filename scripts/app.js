cd4bdffa76d01716c581705a0076328dac194dd3
mohammad reza <mrghkh12@gmail.com>
2024-07-31 19:42:01 +0330

    feat(app.js): lose & score

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,8 @@ const $ = document;
 const ball = new Ball($.getElementById('ball'))
 const playerPaddle = new Paddle($.getElementById('player-paddle'))
 const computerPaddle = new Paddle($.getElementById('computer-paddle'))
+const playerScore = $.getElementById('player-score')
+const computerScore = $.getElementById('computer-score')
 
 
 let lastTime = null
@@ -14,10 +16,27 @@ function updateApp(time){
         const delta = time - lastTime
         ball.update(delta);
         computerPaddle.update(delta, ball.y)
+
+        if(lose()) handleLose()
     }
 
     lastTime = time
     window.requestAnimationFrame(updateApp)
+}
+
+function lose(){
+    const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
+}
+function handleLose(){
+    const rect = ball.rect()
+    if(rect.right >= window.innerWidth){
+        playerScore.textContent = parseInt(playerScore.textContent) + 1
+    }else{
+        computerScore.textContent = parseInt(computerScore.textContent) + 1
+    }
+    ball.reset()
+    computerPaddle.reset()
 }
 
 
