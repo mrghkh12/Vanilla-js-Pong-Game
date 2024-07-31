468f1ac9e38fed075ee5c158ca5594be9828bb7f
mohammad reza <mrghkh12@gmail.com>
2024-07-31 18:44:13 +0330

    feat(ball.js): start direction

--- scripts/ball.js
+++ scripts/ball.js
@@ -21,9 +21,19 @@ export default class Ball{
     rest(){
         this.x = 50;
         this.y = 50; 
+        this.direction = {x : 0}
+        while(Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9){
+            const headeing = randomNumberBetween(0,2 * Math.PI)
+            this.direction = {x : Math.cos(headeing) , y : Math.sin(headeing)}
+        }
+        console.log(this.direction);
     }
 
     update(delta){
         
     }
+}
+
+function randomNumberBetween(min , max){
+    return Math.random() * (max - min) + min
 }
