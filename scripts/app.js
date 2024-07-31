a53e38b4b6b4fd25c89b9925a15f219e653ce6e8
mohammad reza <mrghkh12@gmail.com>
2024-07-31 19:19:37 +0330

    feat(paddle.js): create paddle module , moving player paddle

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
 import Ball from "./ball.js";
+import Paddle from "./paddle.js";
 
 const $ = document;
 
 const ball = new Ball($.getElementById('ball'))
+const playerPaddle = new Paddle($.getElementById('player-paddle'))
+const computerPaddle = new Paddle($.getElementById('computer-paddle'))
 
 
 let lastTime = null
@@ -16,5 +19,10 @@ function updateApp(time){
     lastTime = time
     window.requestAnimationFrame(updateApp)
 }
+
+
+$.addEventListener('mousemove', e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
 
 window.requestAnimationFrame(updateApp)
