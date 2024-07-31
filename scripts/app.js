1b11ba07dfd37700b112488b057e6af7acf6d89b
mohammad reza <mrghkh12@gmail.com>
2024-07-31 17:00:45 +0330

    create ball module

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,27 @@
+<<<<<<< Updated upstream
 
 
 const $ = document;
 
+=======
+<<<<<<< Updated upstream
+=======
+import Ball from "./ball.js";
+
+const $ = document;
+
+const ball = new Ball($.getElementById('ball'))
+
+>>>>>>> Stashed changes
 let lastTime = null
 function updateApp(time){
     if(lastTime){
         const delta = time - lastTime
+<<<<<<< Updated upstream
         console.log(delta);
+=======
+        ball.update(delta);
+>>>>>>> Stashed changes
     }
 
     lastTime = time
@@ -14,3 +29,7 @@ function updateApp(time){
 }
 
 window.requestAnimationFrame(updateApp)
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
---
+++ scripts/ball.js
@@ -0,0 +1,9 @@
+export default class Ball{
+    constructor(ballElem){
+        this.ballElem = ballElem
+    }
+
+    update(delta){
+        
+    }
+}
