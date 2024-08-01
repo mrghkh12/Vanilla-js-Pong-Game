2fac710aabe8b1a7037b656aed989d86ba72d418
mohammad reza <mrghkh12@gmail.com>
2024-08-01 21:45:28 +0330

    style: clean codes

--- scripts/app.js
+++ scripts/app.js
@@ -3,115 +3,118 @@ import Paddle from "./paddle.js";
 
 const $ = document;
 
-let isFinish = false
+let isFinish = false;
 
-const ball = new Ball($.getElementById('ball'))
-const playerPaddle = new Paddle($.getElementById('player-paddle'))
-const computerPaddle = new Paddle($.getElementById('computer-paddle'))
-const playerScore = $.getElementById('player-score')
-const computerScore = $.getElementById('computer-score')
+const ball = new Ball($.getElementById("ball"));
+const playerPaddle = new Paddle($.getElementById("player-paddle"));
+const computerPaddle = new Paddle($.getElementById("computer-paddle"));
+const playerScore = $.getElementById("player-score");
+const computerScore = $.getElementById("computer-score");
 
-let lastTime = null
-function updateApp(time){
-    if(lastTime){
-        const delta = time - lastTime
-        ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
-        computerPaddle.update(delta, ball.y,ball.x , gameDifficulty)
-        changeBgColor(delta)
-        if(lose()) handleLose()
+let lastTime = null;
+function updateApp(time) {
+  if (lastTime) {
+    const delta = time - lastTime;
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+    computerPaddle.update(delta, ball.y, ball.x, gameDifficulty);
+    changeBgColor(delta);
+    if (lose()) handleLose();
+  }
+
+  lastTime = time;
+  if (!isFinish) {
+    window.requestAnimationFrame(updateApp);
+  }
+}
+
+function movePlayerPaddle(e) {
+  playerPaddle.position = (e.y / window.innerHeight) * 100;
+}
+function movePlayerPaddlePhone(e) {
+  playerPaddle.position = (e.touches[0].pageY / window.innerHeight) * 100;
+}
+
+function lose() {
+  const rect = ball.rect();
+  return rect.left > window.innerWidth || rect.right < 0;
+}
+function handleLose() {
+  const rect = ball.rect();
+  if (rect.right >= window.innerWidth) {
+    playerScore.textContent = parseInt(playerScore.textContent) + 1;
+  } else {
+    computerScore.textContent = parseInt(computerScore.textContent) + 1;
+  }
+  ball.reset();
+  computerPaddle.reset();
+  if (
+    gamePoint <= computerScore.innerHTML ||
+    gamePoint <= playerScore.innerHTML
+  ) {
+    isFinish = true;
+    $.removeEventListener("mousemove", movePlayerPaddle);
+    $.removeEventListener("mousemove", movePlayerPaddlePhone);
+    menuWrapper.style.display = "block";
+    difficultyWrapper.innerHTML = "";
+    const selectPointsection = $.querySelector(".action .point");
+    selectPointsection.style.display = "none";
+    playBtn.innerHTML = 'Play Again<span style="color: #ffffff;">&#9654</span>';
+    playBtn.addEventListener("click", () => location.reload());
+    if (playerScore.innerHTML > computerScore.innerHTML) {
+      difficultyWrapper.innerHTML =
+        '<h2>Congratulations, you were able to <span style="color: rgb(79, 202, 79);">Win🎉</span></h2>';
+    } else {
+      difficultyWrapper.innerHTML =
+        '<h2>Sorry, you <span style="color: red;">lost😔</span></h2>';
     }
-
-    lastTime = time
-    if(!isFinish){
-        window.requestAnimationFrame(updateApp)
-    }
-
+  }
 }
 
-function movePlayerPaddle(e){
-    playerPaddle.position = (e.y / window.innerHeight) * 100
-}
-function movePlayerPaddlePhone(e){
-    playerPaddle.position = (e.touches[0].pageY / window.innerHeight) * 100
-}
+function changeBgColor(delta) {
+  let hue = parseFloat(
+    getComputedStyle($.documentElement).getPropertyValue("--hue")
+  );
 
-function lose(){
-    const rect = ball.rect()
-    return rect.left > window.innerWidth || rect.right < 0
+  $.documentElement.style.setProperty("--hue", hue + delta * 0.01);
 }
-function handleLose(){
-    const rect = ball.rect()
-    if(rect.right >= window.innerWidth){
-        playerScore.textContent = parseInt(playerScore.textContent) + 1
-    }else{
-        computerScore.textContent = parseInt(computerScore.textContent) + 1
-    }
-    ball.reset()
-    computerPaddle.reset()
-    if(gamePoint <= computerScore.innerHTML || gamePoint <= playerScore.innerHTML){
-        
-        isFinish = true
-        $.removeEventListener('mousemove', movePlayerPaddle)
-        $.removeEventListener('mousemove', movePlayerPaddlePhone)
-        menuWrapper.style.display = 'block'
-        difficultyWrapper.innerHTML = ''
-        const selectPointsection = $.querySelector('.action .point')
-        selectPointsection.style.display = 'none'
-        playBtn.innerHTML = 'Play Again<span style="color: #ffffff;">&#9654</span>'
-        playBtn.addEventListener('click' , () => location.reload())
-        if(playerScore.innerHTML > computerScore.innerHTML){
-            difficultyWrapper.innerHTML = '<h2>Congratulations, you were able to <span style="color: rgb(79, 202, 79);">Win🎉</span></h2>'
-        }else{
-            difficultyWrapper.innerHTML = '<h2>Sorry, you <span style="color: red;">lost😔</span></h2>'
-        }
-    }
-}
-
-function changeBgColor(delta){
-    let hue = parseFloat(getComputedStyle($.documentElement).getPropertyValue('--hue'))
-
-    $.documentElement.style.setProperty('--hue', hue + delta * 0.01)
-}
-
 
 /////////////dyanmic menu////////////////
 
-let gameDifficulty = 'Normal'
-let gamePoint = 5
+let gameDifficulty = "Normal";
+let gamePoint = 5;
 
-const difficultyWrapper = $.querySelector('.difficulty')
-difficultyWrapper.addEventListener('click' , e => {
-    if(e.target.tagName === 'DIV'){
-      for(let item of difficultyWrapper.children) item.classList.remove('active')
-        
-        e.target.classList.add('active')
-        gameDifficulty = e.target.innerHTML
-    }
-})
+const difficultyWrapper = $.querySelector(".difficulty");
+difficultyWrapper.addEventListener("click", (e) => {
+  if (e.target.tagName === "DIV") {
+    for (let item of difficultyWrapper.children)
+      item.classList.remove("active");
 
-const selectGamePoint = $.querySelector('.select-game-point')
-selectGamePoint.addEventListener('click' , e => {
-    let option = selectGamePoint.querySelector('.option')
-    option.classList.toggle('show')
-    let gamePointElem = selectGamePoint.querySelector('.game-point')
-    if(e.target.tagName == 'P') gamePointElem.innerHTML = e.target.innerHTML
-    gamePoint = gamePointElem.innerHTML
-})
+    e.target.classList.add("active");
+    gameDifficulty = e.target.innerHTML;
+  }
+});
 
-const menuWrapper = $.querySelector('.menu-wrapper')
+const selectGamePoint = $.querySelector(".select-game-point");
+selectGamePoint.addEventListener("click", (e) => {
+  let option = selectGamePoint.querySelector(".option");
+  option.classList.toggle("show");
+  let gamePointElem = selectGamePoint.querySelector(".game-point");
+  if (e.target.tagName == "P") gamePointElem.innerHTML = e.target.innerHTML;
+  gamePoint = gamePointElem.innerHTML;
+});
 
-const playBtn = $.querySelector('.play-btn button')
-playBtn.addEventListener('click' , e => {
+const menuWrapper = $.querySelector(".menu-wrapper");
 
-    menuWrapper.style.display = 'none'
+const playBtn = $.querySelector(".play-btn button");
+playBtn.addEventListener("click", (e) => {
+  menuWrapper.style.display = "none";
 
-    $.addEventListener('mousemove', movePlayerPaddle)
-    $.addEventListener('touchmove', movePlayerPaddlePhone)
-    window.requestAnimationFrame(updateApp)
-})
+  $.addEventListener("mousemove", movePlayerPaddle);
+  $.addEventListener("touchmove", movePlayerPaddlePhone);
+  window.requestAnimationFrame(updateApp);
+});
 
-let loadingWrapper = $.querySelector('.loadingWrapper')
-window.addEventListener('load', e => {
-    
-    loadingWrapper.style.display = 'none'
-})
+let loadingWrapper = $.querySelector(".loadingWrapper");
+window.addEventListener("load", (e) => {
+  loadingWrapper.style.display = "none";
+});
--- scripts/paddle.js
+++ scripts/paddle.js
@@ -1,41 +1,42 @@
-let SPEED
+let SPEED;
 
-export default class Paddle{
-    constructor(paddleElem){
-        this.paddleElem = paddleElem
-        this.reset()
-    }
-    get position(){
-        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue('--position'))
-    }
-    set position(value){
-        this.paddleElem.style.setProperty('--position' , value)
+export default class Paddle {
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem;
+    this.reset();
+  }
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    );
+  }
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value);
+  }
+
+  reset() {
+    this.position = 50;
+  }
+
+  rect() {
+    return this.paddleElem.getBoundingClientRect();
+  }
+
+  update(delta, ballHeight, ballWidth, gameDifficulty) {
+    let computerVision;
+    if (gameDifficulty == "Hard") {
+      computerVision = 50;
+      SPEED = 0.02;
+    } else if (gameDifficulty == "Normal") {
+      computerVision = 60;
+      SPEED = 0.009;
+    } else {
+      computerVision = 70;
+      SPEED = 0.006;
     }
 
-    reset(){
-        this.position = 50
+    if (ballWidth > computerVision) {
+      this.position += SPEED * delta * (ballHeight - this.position);
     }
-
-    rect(){
-        return this.paddleElem.getBoundingClientRect()
-    }
-
-    update(delta , ballHeight, ballWidth , gameDifficulty){
-        let computerVision
-        if(gameDifficulty == 'Hard'){
-            computerVision = 50
-            SPEED = 0.02
-        }else if(gameDifficulty == 'Normal'){
-            computerVision = 60
-            SPEED = 0.009
-        }else{
-            computerVision = 70
-            SPEED = 0.006
-        }
-
-        if(ballWidth > computerVision){
-            this.position += SPEED * delta * (ballHeight - this.position)
-        }
-
-    }
+  }
 }
